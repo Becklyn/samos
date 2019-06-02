05989ef3c027ef6bb65f7553b2525b42e3c97616
Jannik Zschiesche <jz@becklyn.com>
2019-06-02 20:21:49 +0200

    Improve test output

--- tests/compile-all.js
+++ tests/compile-all.js
@@ -1,31 +1,23 @@
 import test from "ava";
 import glob from "glob";
 import sass from "sass";
+import path from "path";
 
+let root = path.join(__dirname, "..");
+let files = glob.sync(`${root}/**/*.scss`);
 
-test.cb("Compile all files", t => {
-    glob(
-        __dirname + "/../{mixins/**/,}*.scss",
-        (err, files) =>
-        {
-            if (err)
-            {
-                t.fail();
-                return;
-            }
-
-            t.assert(files.length > 0);
-
-            files.forEach( file => {
-                // compilation must not fail with errors or deprecations
-                t.notThrows(
-                    () => sass.renderSync({file}),
-                    file
-                );
-            });
-
-            t.end();
-        }
-    );
+test("Files found", t => {
+    t.assert(files.length > 0);
 });
 
+files.forEach(
+    file =>
+    {
+        test(`Compiling: ${path.relative(root, file)}`, t => {
+            t.notThrows(
+                () => sass.renderSync({file}),
+                file
+            );
+        });
+    }
+);
